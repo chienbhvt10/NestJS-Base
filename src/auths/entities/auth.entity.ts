9e7676b19c562954e3ee4078ab28a2f09d26841a
chiendc <chienzxzx33@gmail.com>
2023-08-03 09:46:45 +0700

    .

---
+++ src/auths/entities/auth.entity.ts
@@ -0,0 +1,25 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ObjectId } from 'mongodb';
+import { User } from './user';
+
+@ObjectType()
+@Schema({ id: true })
+export class Auth {
+  id: string;
+
+  @Field(() => String, { nullable: true })
+  @Prop()
+  refreshToken: string;
+
+  @Field(() => String, { nullable: true })
+  accessToken: string;
+
+  @Prop({ type: ObjectId, ref: 'User' })
+  user: User | ObjectId;
+}
+
+export const AuthSchema = SchemaFactory.createForClass(Auth);
+AuthSchema.index({ refreshToken: 1, user: 1 }, { unique: true });
+AuthSchema.index({ refreshToken: 1 }, { unique: true });
+AuthSchema.set('timestamps', true);
