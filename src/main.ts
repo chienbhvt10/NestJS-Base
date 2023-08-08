42f952d01d0d6b3b29b45623417cd0e6b15b359d
chiendc <chienzxzx33@gmail.com>
2023-08-08 09:49:40 +0700

    fix nanoid error

--- src/main.ts
+++ src/main.ts
@@ -13,17 +13,19 @@ async function bootstrap() {
   await app.listen(config().port);
   try {
     const networkInterfaces = os.networkInterfaces();
-    let address = 'localhost';
+    const address = 'localhost';
 
-    for (const name of Object.keys(networkInterfaces)) {
-      for (const net of networkInterfaces[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === 'IPv4' && !net.internal) {
-          address = net.address;
-          break;
-        }
-      }
-    }
+    // let address = 'localhost';
+
+    // for (const name of Object.keys(networkInterfaces)) {
+    //   for (const net of networkInterfaces[name]) {
+    //     // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    //     if (net.family === 'IPv4' && !net.internal) {
+    //       address = net.address;
+    //       break;
+    //     }
+    //   }
+    // }
 
     console.log(
       `🚀🚀 🚀  Server ready and listening at ==> http://${address}:${
